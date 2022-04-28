a3e748b7ab05eb27d078a4cc94f08c29b942bb52
mufeed-vp <mftechnics1@gmail.com>
2022-04-28 08:38:54 +0530

    Add all changes

--- index.js
+++ index.js
@@ -1,41 +1,70 @@
+const logger = require('morgan');
 const express = require('express');
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser');
 
+const env = require('./env')
+
+const registration = require('./services/registration')
 
 const app = express();
 
 
-var originsWhitelist = [
-    'http://localhost:3000', //React Dashboard URL
-];
+app.use(logger('dev'));
+app.use(bodyParser.json({}));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+var cors = require('cors');
+
+var originWhitelist = ['http://localhost:3000']; //react dashboard URL
+
+var corsOptions = {
+  origin: function(origin, callback){
+    var isWhitelisted = originWhitelist.indexOf(origin)!== -1;
+    callback(null, isWhitelisted);
+  },
+  Credential: true
+}
+
+app.use(cors(corsOptions));
 
 
 var db = mongoose.connection;
 
-db.on('connecting', () => console.log('connecting to MongoDB...'));
-
-db.on('error', (error) => {
+try {
   
-  let serverError = `Error in MongoDb connection:  ${error}`
+  db.on('connecting', () => console.log('connecting to MongoDB...'));
   
-  console.log(serverError);
-  mongoose.disconnect();
-});
+  db.on('error', (error) => {
+    
+    let serverError = `Error in MongoDb connection:  ${error}`
+    
+    console.log(serverError);
+    mongoose.disconnect();
+  });
+  
+  db.on('connected', () => console.log('MongoDB connected.'))
+  db.once('open', () => console.log('MongoDB connection opened.'))
+  db.on('reconnected', () => console.log('MongoDB reconnected!'))
+  
+  db.on('disconnected', () => {
+    console.log('MongoDB disconnected, connect again');
+    // mongoose.connect(env.dbString, { useFindAndModify:false, useCreateIndex:true, useNewUrlParser: true, useUnifiedTopology:true });
+  });
+  
+  mongoose.connect(env.dbString, {  useNewUrlParser: true, useUnifiedTopology:true });
+  
+  const server = app.listen(process.env.PORT || 8002, function () {
+    console.log("API server started on http://localhost:" + server.address().port);
+  });
 
-db.on('connected', () => console.log('MongoDB connected.'))
-db.once('open', () => console.log('MongoDB connection opened.'))
-db.on('reconnected', () => console.log('MongoDB reconnected!'))
+} catch (error) {
+  console.log(error)
+}
 
-db.on('disconnected', () => {
-  console.log('MongoDB disconnected, connect again');
-  mongoose.connect(env.dbString, { useFindAndModify:false, useCreateIndex:true, useNewUrlParser: true, useUnifiedTopology:true });
-});
 
-mongoose.connect(env.dbString, { useFindAndModify:false, useCreateIndex:true, useNewUrlParser: true, useUnifiedTopology:true });
-
-const server = app.listen(process.env.PORT || 8000, function () {
-  console.log("API server started on http://localhost:" + server.address().port);
-});
+app.use('/', registration)
 
 module.exports = app
--- models/user.js
+++ models/user.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const Types = mongoose.Schema.Types
+const AutoIncrement = require('mongoose-sequence')(mongoose)
 
 const userSchema = new Schema({
+    userCount : { type: Types.Number, required: true, default: 0 },
     firstName : { type: Types.String, required: true, trim: true },
     lastName : { type: Types.String, required: true, trim: true },
     email : { type: Types.String, required: true, trim: true },
-    joiningDate: { type: Schema.Types.Date, required: true },
+    registeredDate: { type: Schema.Types.Date, required: true },
 },{ versionKey: false })
 
+productSchema.plugin(AutoIncrement,{ inc_field: 'userCount' });
 module.exports = mongoose.model('user', userSchema)
