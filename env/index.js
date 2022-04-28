80dfca08fce8bebd956ad23c5e1912d827b91f5c
Mufeed Valiya peedikayil <54625663+mufeed-vp@users.noreply.github.com>
2022-04-28 23:50:28 +0530

    Delete index.js

--- env/index.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-
-  // Database connection string
-  // dbString: 'mongodb://localhost:27017/poumki'
-  dbString: 'mongodb+srv://mufeedvp:9446652245mf123@poumki.nghlc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-
-}
