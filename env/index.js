935c64ee89769b5be307e48158c8986d6980afc1
mufeed-vp <mftechnics1@gmail.com>
2022-05-22 14:25:20 +0530

    add env folder

--- env/index.js
+++ env/index.js
@@ -1,6 +1,7 @@
 
 module.exports = {
 
+  dbString: 'mongodb+srv://mufeedvp:<94466522245mf123>@poumki.nghlc.mongodb.net/?retryWrites=true&w=majority'
   // Database connection string
   // dbString: 'mongodb://localhost:27017/poumki'
 
